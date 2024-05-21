7cd5f9857813675a1ee120349bf68f01fbeefecb
ArNGiH <aryannayar47@gmail.com>
2024-05-21 16:10:36 +0530

    added more styles to homepage

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -13,10 +13,7 @@ function HomePage() {
         <div className="wrapper">
           <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            explicabo suscipit cum eius, iure est nulla animi consequatur
-            facilis id pariatur fugit quos laudantium temporibus dolor ea
-            repellat provident impedit!
+            Ghar lelo bc
           </p>
           <SearchBar />
           <div className="boxes">

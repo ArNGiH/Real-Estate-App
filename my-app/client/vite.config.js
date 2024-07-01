c46f8bdd6fdd1fab97f2ddfec3f589fe904c81ef
ArNGiH <aryannayar47@gmail.com>
2024-07-01 10:40:52 +0530

    Update build configuration

--- my-app/client/vite.config.js
+++ my-app/client/vite.config.js
@@ -1,10 +1,9 @@
-// my-app/client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'  // This sets the output directory to 'dist'
+    outDir: 'dist'
   }
 });
